dea383cf070f67c9851767198a388487aa841fde
Hoomanbeans <66372206+Hoomanbeans@users.noreply.github.com>
2022-10-31 17:57:07 +0000

    1st test commit

---
+++ Source/SkyFrontier/SkyFrontier.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SkyFrontier.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SkyFrontier, "SkyFrontier" );
