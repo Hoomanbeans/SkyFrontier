6fc494ac948270864c378e8f0c18f375defd3c63
Genexuz <85886630+Genexuz@users.noreply.github.com>
2022-12-02 10:01:00 +0000

    Revert "Delete HealthSystem.cpp"
    This reverts commit 8cddb64b5325cb3ba9f491211c97293cc048fc22.

---
+++ Source/SkyFrontier/Private/HealthSystem.cpp
@@ -0,0 +1,64 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HealthSystem.h"
+
+UHealthSystem::UHealthSystem()
+{
+	// This is a component that doesn't need a tick so lets disable it
+	PrimaryComponentTick.bCanEverTick = false;
+	PrimaryComponentTick.bStartWithTickEnabled = false;
+}
+
+void UHealthSystem::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// This is here for now, but if you ever do anything serialization related you might not want this.
+	Health = MaxHealth;
+}
+
+float UHealthSystem::GetHealth() const
+{
+	return Health;
+}
+
+float UHealthSystem::GetMaxHealth() const
+{
+	return MaxHealth;
+}
+
+float UHealthSystem::GetHealthAsPercentage() const
+{
+	return Health / MaxHealth;
+}
+
+void UHealthSystem::ModifyHealth(const float Amount)
+{
+	if(Amount == 0)
+		return;
+	
+	Health += Amount;
+
+	Amount > 0 ? OnDamageHealedEvent.Broadcast(Amount) : OnDamageTakenEvent.Broadcast(Amount);
+}
+
+void UHealthSystem::TakeDamage(const float Amount)
+{
+	if(Amount > 0)
+	{
+		Health -= Amount;
+
+		OnDamageTakenEvent.Broadcast(Amount);
+	}
+}
+ 
+void UHealthSystem::RecoverHealth(const float Amount)
+{
+	if(Amount > 0)
+	{
+		Health += Amount;
+
+		OnDamageHealedEvent.Broadcast(Amount);
+	}
+}
